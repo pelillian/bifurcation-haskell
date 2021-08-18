1cb7c2de94031ad43dd657d705f734a2836d9669
pelillian <plillian@usc.edu>
2021-08-17 23:58:38 -0700

    Plot last n iterations and speed up

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,26 @@
 import Graphics.UI.GLUT
-import System.Exit
 
 width, height :: GLint
 width = 1920
 height = 1080
 
+iters = 1000
+points = 20
+
 logisticMap :: GLfloat -> GLfloat -> GLfloat
 logisticMap r x = r * x * (1 - x)
 
-iterMap :: Int -> GLfloat -> GLfloat
-iterMap n r = (iterate (logisticMap r) (1e-5 * r)) !! n
+iterMap :: Int -> GLfloat -> [GLfloat]
+iterMap n r = drop (n - points) $ take n $ iterate (logisticMap r) 1e-4
+
+range :: [GLfloat]
+range = [1,1.0001..4]
+
+x = concatMap (replicate points) range
+y = concatMap (iterMap iters) range
 
 myPoints :: [(GLfloat,GLfloat,GLfloat)]
---myPoints = [ (iterate logisticMap r !! 1000, r, 0) | r <- [0,0.001..4] ]
-myPoints = [ ((r - 2.5) / 1.8, (iterMap 1000 r) - 0.5, 0) | r <- [1,1.00001..4] ]
+myPoints = zip3 (map (* 0.5) $ map (subtract 2.5) x) (map (subtract 0.5) y) (cycle [0])
 
 main :: IO ()
 main = do
@@ -24,7 +31,7 @@ main = do
   mainLoop
 
 display :: DisplayCallback
-display = do 
+display = do
   clear [ColorBuffer]
   renderPrimitive Points $
      mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
